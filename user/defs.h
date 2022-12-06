a063810e3462a36e3835db7a62e2ea1730e1d767
Stanislav Kramar <stanislav.k@gowombat.team>
2022-12-06 01:27:35 +0100

    add user-defined constants

---
+++ user/defs.h
@@ -0,0 +1,14 @@
+#ifndef _XV6_USER_DEFS_H_
+#define _XV6_USER_DEFS_H_
+
+#define EXIT_SUCCESS 0
+#define EXIT_FAILURE 1
+
+#define STDIN 0
+#define STDOUT 1
+#define STDERR 2
+
+#define INT_MAX 1 << (sizeof(int) * 8 - 1)
+
+#endif // _XV6_USER_DEFS_H_
+
